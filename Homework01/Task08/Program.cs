73b770cfad33ba0b2c6f4864e0fda13f6dbbcd76
Daniil Rassadin <daniil@rassadin.org>
2022-11-29 20:23:06 +0600

    Task08 done

---
+++ Homework01/Task08/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int index = 1;
+
+switch (index > number)
+{
+    case true:
+        while (index > number)
+        {
+            if (index % 2 == 0) { Console.Write($"{index}, "); }
+            index--;
+        }
+        break;
+    case false:
+        while (index < number)
+        {
+            if (index % 2 == 0) { Console.Write($"{index}, "); }
+            index++;
+        }
+        break;
+}
+
+
