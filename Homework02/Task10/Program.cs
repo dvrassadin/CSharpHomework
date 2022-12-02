b66d592397304e29010004a0f77943393ca9a084
Daniil Rassadin <daniil@rassadin.org>
2022-12-02 15:50:34 +0600

    Task10 done

---
+++ Homework02/Task10/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int? ShowSecondNumber(int threeDigitNumber)
+{
+    int? secondNumber = null;
+    if ((threeDigitNumber >= -999 && threeDigitNumber <= -100) || (threeDigitNumber <= 999 && threeDigitNumber >= 100))
+    {
+        secondNumber = threeDigitNumber / 10 % 10;
+        if (secondNumber < 0)
+            secondNumber = -secondNumber;
+    }
+    
+    return secondNumber;
+}
+
+Console.WriteLine("Введите трёхзначное число: ");
+var secondNumber = ShowSecondNumber(Convert.ToInt32(Console.ReadLine()));
+
+if (secondNumber != null)
+    Console.WriteLine($"Вторая цифра: {secondNumber}.");
+else
+    Console.WriteLine("Это не трёхзначное число.");
+
