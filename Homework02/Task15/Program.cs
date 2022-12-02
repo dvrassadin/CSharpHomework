1bb9bd987adb4d2ab42a46d0b0fbfa72947b7048
Daniil Rassadin <daniil@rassadin.org>
2022-12-02 16:19:15 +0600

    Task15 done

--- Homework02/Task15/Program.cs
+++ Homework02/Task15/Program.cs
@@ -3,3 +3,22 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+
+string? ShowDayName(int dayNumber)
+{
+    string? dayName = null;
+    if (dayNumber >= 1 && dayNumber <= 5)
+        dayName = "weekday";
+    else if (dayNumber == 6 || dayNumber == 7)
+        dayName = "weekend";
+    
+    return dayName;
+}
+
+Console.Write("Enter day number: ");
+var dayName = ShowDayName(Convert.ToInt32(Console.ReadLine()));
+
+if (dayName != null)
+    Console.WriteLine($"It's a {dayName}.");
+else
+    Console.WriteLine("Такого дня нет.");
