86552d1bbe011e76b2346e266c423220e4da220d
Daniil Rassadin <daniil@rassadin.org>
2022-12-02 17:07:24 +0600

    Task13 done

--- Homework02/Task13/Program.cs
+++ Homework02/Task13/Program.cs
@@ -2,3 +2,27 @@
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
+
+int? FindThirdNumber(int? number)
+{
+    if (number < 0)
+        number = -number;
+    if (number > 99)
+    {
+        while (number > 1000)
+            number /= 10;
+        number %= 10;
+    }
+    else
+        number = null;
+
+    return number;
+}
+
+Console.Write("Введите число: ");
+var thirdNumber = FindThirdNumber(Convert.ToInt32(Console.ReadLine()));
+
+if (thirdNumber != null)
+    Console.WriteLine($"Третья цифра: {thirdNumber}.");
+else
+    Console.WriteLine("Третьей цифры нет.");
