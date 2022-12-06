a7cc7849a7e1e42887b06de83ea54d0567461f5e
Daniil Rassadin <daniil@rassadin.org>
2022-12-06 17:34:00 +0600

    Task19 done

--- Homework03/Task19/Program.cs
+++ Homework03/Task19/Program.cs
@@ -3,3 +3,45 @@
 // 14212 -> нет
 // 12821 -> да
 // 23432 -> да
+
+int CountOfNumber(int number)
+{
+    var countOfNumbers = 0;
+
+    if (number < 0) number = -number;
+
+    while (number > 0)
+    {
+        number /= 10;
+        countOfNumbers++;
+    }
+
+    return countOfNumbers;
+}
+
+bool IsPalindrome(int number) // Принимает любое число
+{
+    var isPalindrome = true;
+    for (var i = Convert.ToInt32(Math.Pow(10, CountOfNumber(number) - 1)); i >= 10; i /= 100)
+    {
+        if (number / i == number % 10)
+        {
+            number -= number / i * i;
+            number /= 10;
+        }
+        else
+        {
+            isPalindrome = false;
+            break;
+        }
+    }
+    return isPalindrome;
+}
+
+Console.Write("Введите число: ");
+var number = Convert.ToInt32(Console.ReadLine());
+
+if (IsPalindrome(number))
+    Console.WriteLine("Палиндром.");
+else
+    Console.WriteLine("Не палиндром");
