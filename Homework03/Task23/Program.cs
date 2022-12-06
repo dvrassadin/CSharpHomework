7d913d1df91bdf4cde15e916c4aba308e9b1b8d9
Daniil Rassadin <daniil@rassadin.org>
2022-12-06 15:44:46 +0600

    Task23 done

---
+++ Homework03/Task23/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Write("Введите число: ");
+var number = Convert.ToDouble(Console.ReadLine());
+
+if (number > 0)
+    for (var i = 1; i <= number; i++)
+        Console.Write($"{Math.Pow(i, 3)}, ");
+else
+    for (var i = 1; i >= number; i--)
+        Console.Write($"{Math.Pow(i, 3)}, ");
