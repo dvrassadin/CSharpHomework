30621552f10e283b35b2f15176a562f50ecc816d
Daniil Rassadin <daniil@rassadin.org>
2022-12-13 16:11:14 +0600

    Start homework05

---
+++ Homework05/Task36/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] CreateArray(uint size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    return array;
+}
+
