85bd25d6f62344a396cff58e3c45fa6534054e7f
Daniil Rassadin <daniil@rassadin.org>
2022-12-13 16:13:11 +0600

    Start homework05

---
+++ Homework05/Task34/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу,
+// которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] CreateArray(uint size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    return array;
+}
+
