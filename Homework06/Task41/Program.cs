ea9bd21d4d7aa8c6a35bed1da24cf756230e2387
Daniil Rassadin <daniil@rassadin.org>
2022-12-16 16:50:55 +0600

    Homework done

---
+++ Homework06/Task41/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int[] CreateArray()
+{
+    Console.Write("Введите количество чисел: ");
+    int[] array = new int[Convert.ToInt32(Console.ReadLine())];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите {i + 1} из {array.Length} число: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+
+    return array;
+}
+
+int CountOfPositive(int[] array)
+{
+    var count = 0;
+    foreach (int number in array)
+        if (number > 0)
+            count++;
+
+    return count;
+}
+
+
+int[] array = CreateArray();
+
+Console.WriteLine($"Вы ввели {CountOfPositive(array)} чисел больше нуля.");
---
+++ Homework06/Task43/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double[] IntersectionPoint(double b1, double k1, double b2, double k2)
+{
+    double[] point = new double[2] { (b2 - b1) / (k1 - k2), 0 };
+    point[1] = k1 * point[0] + b1;
+
+    return point;
+}
+
+
+
+Console.WriteLine("Введите координаты первой прямой:");
+var b1 = Convert.ToDouble(Console.ReadLine());
+var k1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите координаты второй прямой:");
+var b2 = Convert.ToDouble(Console.ReadLine());
+var k2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine($"Точка пересечения: ({String.Join("; ", IntersectionPoint(b1, k1, b2, k2))}).");
