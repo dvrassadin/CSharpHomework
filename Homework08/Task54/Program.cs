8f0f588617ef9fe75f64f1bb645e946c33e98e5d
Daniil Rassadin <daniil@rassadin.org>
2022-12-26 00:30:22 +0600

    Task64 done

---
+++ Homework08/Task54/Program.cs
@@ -0,0 +1,80 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу,
+// которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($" | {array[i, j]} | ");
+        Console.WriteLine();
+    }
+}
+
+int[,]? CreateIntArray(int rows, int columns, int minValue, int maxValue)
+{
+    if (rows <= 0 || columns <= 0)
+        return null;
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            array[i, j] = new Random().Next(minValue, maxValue + 1);
+
+    return array;
+}
+
+int[,] SortElementsInRows(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+                Console.WriteLine(array[i, k]);
+                PrintArray(array);
+                Console.WriteLine();
+            }
+
+    return array;
+}
+
+// int[,] SortElementsInRows(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+
+//         }
+
+//     }
+
+//     return array;
+// }
+
+
+if (CreateIntArray(1, 8, 0, 9) is int[,] array)
+{
+    PrintArray(array);
+    Console.WriteLine();
+    SortElementsInRows(array);
+    PrintArray(array);
+}
+else
+    Console.WriteLine("Такой массив нельзя создать.");
+
+
+
---
+++ Homework08/Task58/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($" | {array[i, j]} | ");
+        Console.WriteLine();
+    }
+}
+
+int[,]? CreateIntArray(int rows, int columns, int minValue, int maxValue)
+{
+    if (rows <= 0 || columns <= 0)
+        return null;
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            array[i, j] = new Random().Next(minValue, maxValue + 1);
+
+    return array;
+}
+
+int[,]? MatrixProduct(int[,] firstMatrix, int[,] secondMatrix)
+{
+    if (firstMatrix.GetLength(1) != secondMatrix.GetLength(0))
+        return null;
+
+    int[,] product = new int[firstMatrix.GetLength(0), firstMatrix.GetLength(1)];
+
+    for (int i = 0; i < product.GetLength(0); i++)
+    {
+        for (int j = 0; j < product.GetLength(1); j++)
+        {
+
+        }
+
+    }
+
+    return product;
+}
+
+int[,] firstMatrix = { { 2, 4 }, { 3, 2 } };
+int[,] secondMatrix = { { 3, 4 }, { 3, 3 } };
+
+if (MatrixProduct(firstMatrix, secondMatrix) is int[,] array)
+    PrintArray(array);
+else
+    Console.WriteLine("Такие матрицы нельзя перемножить.");
