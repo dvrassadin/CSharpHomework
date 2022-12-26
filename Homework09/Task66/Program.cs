c9fe6546dd57eb98b515cf644dc518136691f99f
Daniil Rassadin <daniil@rassadin.org>
2022-12-26 12:39:33 +0600

    Task 68 done

--- Homework09/Task66/Program.cs
+++ Homework09/Task66/Program.cs
@@ -2,3 +2,11 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
+int SumBetween(int m, int n)
+{
+    if (m == n) return m;
+
+    return m > n ? m + SumBetween(m - 1, n) : m + SumBetween(m + 1, n);
+}
+
+Console.WriteLine(SumBetween(m: 1, n: -2));
